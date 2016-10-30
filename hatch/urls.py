a8e72df3052daa623139da284032738436909460
Matt Rosendin <matt.rosendin@gmail.com>
2016-10-30 15:51:28 -0700

    restart with a flask app

--- hatch/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-import home
-
-urlpatterns = [
-    url(r'^$', include('home.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-]
