bbdea0aa514a223244082cc2bd3682a235fc5ec1
Matt Rosendin <matt.rosendin@gmail.com>
2016-10-30 17:13:11 -0700

    add generator and docs templates

--- app/views.py
+++ app/views.py
@@ -4,3 +4,11 @@ from app import app
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/generator')
+def generator():
+    return render_template('generator.html')
+
+@app.route('/docs')
+def docs():
+    return render_template('docs.html')
