12ac8fa5730737baa87fb302866e5a6ce8279d91
Matt Rosendin <matt.rosendin@gmail.com>
2016-11-01 21:22:47 -0700

    add generatorController

---
+++ app/static/js/generator.js
@@ -0,0 +1,10 @@
+(function() {
+
+  'use strict';
+
+  angular.module('Generator', [])
+  .controller('generatorController', ['$scope', '$log', function($scope, $log) {
+    // $log.log("Test");
+  }]);
+
+}());
