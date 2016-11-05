16e75e37dd612f7b2516db07eddf058ec635ecb1
Matt Rosendin <matt.rosendin@gmail.com>
2016-11-05 12:09:28 -0700

    add gulp-sass, remove unused gulp modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require("gulp");
+var sass = require('gulp-sass');
 
-gulp.task("default", function() {
-
+gulp.task('sass', function() {
+  return gulp.src('./app/static/css/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./app/static/css'));
 });
+
+gulp.task('watch', function() {
+  gulp.watch('./app/static/css/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
