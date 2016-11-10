c7771218559e4352339450afee78983961da03dd
Matt Rosendin <matt.rosendin@gmail.com>
2016-11-10 08:01:57 -0800

    add basic api file and tests dir

---
+++ app/api.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+models = {
+  'example_model_1': {'name': 'users'},
+  'example_model_2': {'name': 'organizations'}
+}
+
+class Model(Resource):
+  def get(self, model_id):
+    return models[model_id]
+
+  def put(self, model_id):
+    args = parser.parse_args()
+    model = {'name': args['name']}
+    models[model_id] = model
+    return model, 201
+
+  def delete(self, model_id):
+    del models[model_id]
+    return '', 204
+
+class ModelList(Resource):
+  def get(self):
+    return models
+
+  def post(self):
+    args = parser.parse_args()
+    model_id = int(max(models.keys())).lstrip('model')) + 1
+    model_id = 'example_model_%i' % model_id
+    models[model_id] = {'name': args['name']}
+    return models[model_id], 201
+
+api.add_resource(ModelList, '/models')
+api.add_resource(Model, '/models/<model_id>')
