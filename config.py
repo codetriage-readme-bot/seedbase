3d534205d8134d1c3c81ba61af7e2eaee9ffb6d2
Matt Rosendin <matt.rosendin@gmail.com>
2016-11-12 22:25:40 -0800

    add db config

--- config.py
+++ config.py
@@ -3,9 +3,13 @@ import os
 from dotenv import load_dotenv
 
 # Load dotenv for environment variables
-dotenv_path = os.join(os.dirname(__file__), '.env')
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
+
+MYSQL_USERNAME = os.environ.get("MYSQL_USERNAME")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE")
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = 'mysql://username:password@localhost/db_name'
+SQLALCHEMY_DATABASE_URI = 'mysql://' + MYSQL_USERNAME + ':' + MYSQL_PASSWORD + '@localhost/' + MYSQL_DATABASE
