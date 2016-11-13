b917e5db37212aa4bcbab95bf91688efcab31501
Matt Rosendin <matt.rosendin@gmail.com>
2016-11-12 22:20:36 -0800

    add env configuration for mysql

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 # Define the application directory
 import os
+from dotenv import load_dotenv
+
+# Load dotenv for environment variables
+dotenv_path = os.join(os.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATABASE_URI = 'mysql://username:password@localhost/db_name'
