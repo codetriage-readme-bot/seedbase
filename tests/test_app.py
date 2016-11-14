4ef0ff3ef59ea391abc969b3790231f6ac6c2415
Matt Rosendin <matt.rosendin@gmail.com>
2016-11-13 18:14:54 -0800

    update app test file

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,20 +5,22 @@ import tempfile
 
 # Append the parent directory so we can import app module
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-import app
+from app import app, db
 
 class HatchTestCase(unittest.TestCase):
 
   def setUp(self):
-    self.db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
-    app.app.config['TESTING'] = True
-    self.app = app.app.test_client()
-    with app.app.app_context():
-      app.init_db()
+    self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+    app.config['TESTING'] = True
+    self.app = app.test_client()
 
   def tearDown(self):
     os.close(self.db_fd)
-    os.unlink(app.app.config['DATABASE'])
+    os.unlink(app.config['DATABASE'])
+
+  def test_root_page(self):
+    rv = self.app.get('/')
+    assert b'Never manually create testing data again.' in rv.data
 
 if __name__ == '__main__':
   unittest.main()
