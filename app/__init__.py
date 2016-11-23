199641b3a9fdbd9ce958f16cdbff3e1a8bebed36
Matt Rosendin <matt.rosendin@gmail.com>
2016-11-22 23:13:14 -0800

    config file load from env var

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,13 @@ from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-app.config.from_object('config.default')
-app.config.from_envvar('HATCH_CONFIG', silent=True)
+app.config.from_object(os.environ['HATCH_CONFIG'])
 
 login_manager = LoginManager()
 login_manager.init_app(app)
