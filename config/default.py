acd507e6494686d914688bb94027b4ca868d3048
Matt Rosendin <matt.rosendin@gmail.com>
2017-01-22 21:54:42 -0800

    Use MySQL instead of PostgreSQL

--- config/default.py
+++ config/default.py
@@ -15,10 +15,10 @@ TEMPLATES_AUTO_RELOAD = True
 
 WTF_CSRF_CHECK_DEFAULT = False
 
-PG_USERNAME = os.environ.get("PG_USERNAME")
-PG_PASSWORD = os.environ.get("PG_PASSWORD")
-PG_DATABASE = os.environ.get("PG_DATABASE")
+MYSQL_USERNAME = os.environ.get("MYSQL_USERNAME")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE")
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = 'postgresql://'+PG_USERNAME+':'+PG_PASSWORD+'@localhost/' + PG_DATABASE
+SQLALCHEMY_DATABASE_URI = 'mysql://'+MYSQL_USERNAME+':'+MYSQL_PASSWORD+'@localhost/' + MYSQL_DATABASE
--- config/production.py
+++ config/production.py
@@ -7,6 +7,6 @@ DEBUG=False
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = os.environ['CLEARDB_DATABASE_URL']
 SECRET_KEY = os.environ['SECRET_KEY']
 WTF_CSRF_CHECK_DEFAULT = False
